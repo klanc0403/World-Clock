6a62e9c485bb962d782d6aedaef93a2f287c399f
klanc0403 <kelly@kellyisasinger.com>
2026-03-16 16:11:49 -0400

    injected JS functions for dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-function showSelectedCountry(event){
-    
+function displayCityTime(event) {
+  let city = event.target.value;
+  let cityTimeElement = document.querySelector("#city-time");
+
+  if (city === "select") {
+    cityTimeElement.innerHTML = "👉 City time here 👈";
+    return;
+  }
+
+  let timeZone = "";
+
+  if (city === "Paris") {
+    timeZone = "Europe/Paris";
+  } else if (city === "Tokyo") {
+    timeZone = "Asia/Tokyo";
+  } else if (city === "Sydney") {
+    timeZone = "Australia/Sydney";
+  }
+
+  let cityTime = moment().tz(timeZone).format("dddd, MMMM D, YYYY h:mm:ss A");
+
+  cityTimeElement.innerHTML = `It is ${cityTime} in ${city}`;
 }
 
-let countriesSelect = document.querySelector("#countries");
-countriesSelect.addEventListener("change", showSelectedCountry);
+let countriesElement = document.querySelector("#countries");
+countriesElement.addEventListener("change", displayCityTime);
